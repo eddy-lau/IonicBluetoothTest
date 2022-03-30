518d4d7b1c0d1bcdcf81dba389fb76c7c8617347
Eddie Lau <eddie@touchutility.com>
2022-03-30 14:37:02 +0800

    added android implementation.

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -2,51 +2,130 @@
   <ion-page>
     <ion-header :translucent="true">
       <ion-toolbar>
-        <ion-title>Blank</ion-title>
+        <ion-title>Bluetooth Test</ion-title>
+        <ion-buttons slot="end">
+          <ion-button v-if="!state.scanning" @click="scan">Scan</ion-button>
+          <ion-button v-if="state.scanning" @click="stop">Stop</ion-button>
+        </ion-buttons>
       </ion-toolbar>
     </ion-header>
     
     <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Blank</ion-title>
-        </ion-toolbar>
-      </ion-header>
-    
       <div id="container">
-        <strong>Ready to create an app?</strong>
-        <p>Start with Ionic <a target="_blank" rel="noopener noreferrer" href="https://ionicframework.com/docs/components">UI Components</a></p>
+        <ion-list>
+          <ion-item v-for="device in state.devices" v-bind:key="device.deviceId" @click="connect(device)">
+            <ion-label>{{device.deviceId}} {{device.name}}</ion-label>
+            <ion-spinner v-if="device.state===1"/>
+            <span v-if="device.state===2">✓</span>
+          </ion-item>
+        </ion-list>
       </div>
     </ion-content>
   </ion-page>
 </template>
 
 <script lang="ts">
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
-import { defineComponent } from 'vue';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButton, IonButtons, IonList, IonItem, IonLabel, alertController, IonSpinner } from '@ionic/vue';
+import { defineComponent, reactive } from 'vue';
+import { BleClient, BleDevice } from '@capacitor-community/bluetooth-le';
+
+enum DeviceState {
+  unconnected = 0,
+  connecting = 1,
+  connected = 2
+}
+
+interface MyBleDevice extends BleDevice {
+  state: DeviceState
+}
+
+interface State {
+  scanning:boolean
+  devices: { [deviceId:string]: MyBleDevice }
+}
 
 export default defineComponent({
   name: 'HomePage',
+  setup() {
+
+    let state = reactive<State>({scanning:false, devices:{}})
+    
+    return {
+      state
+    }
+  },
+  async created() {
+    await BleClient.initialize({ androidNeverForLocation: true });
+  },
+  methods: {
+    async scan() {
+
+      await BleClient.requestLEScan({services: [],},
+        (result) => {
+          this.state.devices[result.device.deviceId] = {state:DeviceState.unconnected, ...result.device}
+        }
+      );
+      this.state.devices = {}
+      this.state.scanning = true
+
+      setTimeout(this.stop, 5000);
+
+    },
+    async stop() {
+      await BleClient.stopLEScan()
+      this.state.scanning = false
+    },
+    async connect(device:BleDevice) {
+      try {
+        this.state.devices[device.deviceId].state = DeviceState.connecting
+        await BleClient.connect(device.deviceId, this.onDisconnected)
+        this.state.devices[device.deviceId].state = DeviceState.connected
+        this.showSuccess(device)
+      } catch( error ) {
+        this.state.devices[device.deviceId].state = DeviceState.unconnected
+        this.showError(error as Error)
+      }
+    },
+    onDisconnected(deviceId:string) {
+      this.state.devices[deviceId].state = DeviceState.unconnected
+    },
+    async showSuccess(device:BleDevice) {
+        const alert = await alertController
+          .create({
+            header: 'Connected',
+            message: `Successfully conntected to device ${device.deviceId}`,
+            buttons: ['OK'],
+          });
+        await alert.present();        
+    },
+    async showError(error:Error) {
+        const alert = await alertController
+          .create({
+            header: 'Error',
+            message: error.message,
+            buttons: ['OK'],
+          });
+        await alert.present();        
+    }
+
+  },
   components: {
     IonContent,
     IonHeader,
     IonPage,
     IonTitle,
-    IonToolbar
+    IonToolbar,
+    IonButton,
+    IonButtons,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonSpinner
   }
 });
 </script>
 
 <style scoped>
-#container {
-  text-align: center;
-  
-  position: absolute;
-  left: 0;
-  right: 0;
-  top: 50%;
-  transform: translateY(-50%);
-}
 
 #container strong {
   font-size: 20px;

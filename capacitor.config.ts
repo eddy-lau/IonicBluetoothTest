59475d584e402c3515a40487b4229e519a3bde44
Eddie Lau <eddie@touchutility.com>
2022-03-30 11:08:45 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'BluetoothTest',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
